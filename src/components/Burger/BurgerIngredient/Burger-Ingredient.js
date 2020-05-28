92741511a958218b31171d98c5f530d6705e33c3
Carl Olson <colson9@student.cscc.edu>
2020-05-28 14:50:24 -0400

    run npm audit fix

---
+++ src/components/Burger/BurgerIngredient/Burger-Ingredient.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import classes from './burger-ingredient.css';
+
+
+const burgerIngredient = (props) => {
+  let ingredient = null;
+
+  switch (props.type) {
+    case ('bread-bottom'):
+      ingredient = <div className={classes.BreadBottom}></div>;
+      break;
+      case('bread-top'):
+      ingredient= (
+        <div className={classes.BreadTop}>
+          <div className={classes.Seeds1}></div>
+          <div className={classes.Seeds2}></div>
+        </div>
+      ); 
+      break;
+
+
+
+  }
+
+};
+
+
+export default burgerIngredient;
