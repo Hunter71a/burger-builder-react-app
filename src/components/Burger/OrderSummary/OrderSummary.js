27f0e88d2d44fed2b48602cb05b4c51f4f60fb3a
Carl Olson <colson9@student.cscc.edu>
2020-06-01 12:31:07 -0400

    Update drawer toggle

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,64 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Button from '../../UI/Button/Button';
 import Combiner from '../../../hoc/Auxillary';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-      return (<li key={igKey}>
-        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
-      </li>
-      );
-    });
+class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log('[OrderSummary] WillUpdate');
+  }
 
-  return (
-    <Combiner>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: ${props.price}</strong></p>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Combiner>
-  );
+  render() {
 
-};
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(igKey => {
+        return (<li key={igKey}>
+          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+        </li>
+        );
+      });
+    return (
+      <Combiner>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: ${this.props.price}</strong></p>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Combiner>
+    );
 
-export default orderSummary;
+  }
+}
+
+export default OrderSummary;
+
+  // }= (props) => {
+  //   const ingredientSummary = Object.keys(props.ingredients)
+  //     .map(igKey => {
+  //       return (<li key={igKey}>
+  //         <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+  //       </li>
+  //       );
+  //     });
+
+  //   return (
+  //     <Combiner>
+  //       <h3>Your Order</h3>
+  //       <p>A delicious burger with the following ingredients:</p>
+  //       <ul>
+  //         {ingredientSummary}
+  //       </ul>
+  //       <p><strong>Total Price: ${props.price}</strong></p>
+  //       <p>Continue to checkout?</p>
+  //       <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+  //       <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+  //     </Combiner>
+  //   );
+//
+//};
+
+// export default orderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
 import Backdrop from '../Backdrop/Backdrop';
 import classes from './Modal.css';
 import Combiner from '../../../hoc/Auxillary';
 
-const modal = (props) => (
- <Combiner>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0'
-        }}>
-        {props.children}
-      </div>
- 
-  </Combiner>
-);
+class Modal extends Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate () {  // demonstrate unnecessary updating is prevented
+    console.log('Modal component will update now!')
+  }
 
 
-export default modal;
+  render() {
+    return (
+      <Combiner>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+
+      </Combiner>
+    );
+  }
+}
+
+export default Modal;
+
+// convert to class based component to implement method to reduce uneccesary
+// rerender cycles
+// }= (props) => (
+//  <Combiner>
+//     <Backdrop show={props.show} clicked={props.modalClosed} />
+//       <div
+//         className={classes.Modal}
+//         style={{
+//           transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+//           opacity: props.show ? '1' : '0'
+//         }}>
+//         {props.children}
+//       </div>
+
+//   </Combiner>
+//);
+
+
+// export default modal;
 
